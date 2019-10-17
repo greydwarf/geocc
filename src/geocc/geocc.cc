78b0196ccf87b85385c0f1f892b9db77a5dc75d3
Bryce Sellers <bryce.sellers@gmail.com>
2019-10-17 07:46:56 -0600

    Use libgeos for geo indexing

--- src/geocc/geocc.cc
+++ src/geocc/geocc.cc
@@ -1,10 +1,8 @@
 #include "world_generated.h"
-#include "s2/s2shape_index.h"
-#include "s2/mutable_s2shape_index.h"
-#include "s2/s2contains_point_query.h"
-#include "s2/s2polygon.h"
-#include "s2/s2builderutil_s2polygon_layer.h"
-#include <stdio.h>
+#include "geos_c.h"
+#include "zstd.h"
+#include <cstdio>
+#include <stdarg.h>
 #include <string>
 #include <cassert>
 #include <unordered_map>
@@ -14,78 +12,134 @@ struct Country {
    std::string fips, iso3, name;
 };
 
+char* NOTICE=const_cast<char*>("NOTICE");
+char* ERROR=const_cast<char*>("ERROR");
+GEOSContextHandle_t ctx;
+
+void geosMessageHandler(const char*message, void* prefix) {
+   const char* p = static_cast<char*>(prefix);
+   printf("GEOS %s - %s\n", p, message);
+}
+
 std::vector<std::unique_ptr<Country>> country_vec;
-std::unordered_map<uint32_t, Country*> countries;
-S2ShapeIndex* readWorldMap(FILE *f) {
-   auto ret = new MutableS2ShapeIndex();
-   uint32_t id = 0;
+
+struct RegionEntry {
+   GEOSGeometry *geom;
+   const GEOSPreparedGeometry *prepared;
+   const Country* country;
+};
+
+GEOSSTRtree* readWorldMap(FILE *f) {
+   GEOSContext_setNoticeMessageHandler_r(ctx, geosMessageHandler, static_cast<void*>(NOTICE));
+   GEOSContext_setErrorMessageHandler_r(ctx, geosMessageHandler, static_cast<void*>(ERROR));
+   auto ret = GEOSSTRtree_create_r(ctx, 10);
+   auto geosReader = GEOSWKTReader_create_r(ctx);
    while (true) {
-      int32_t sz;
-      auto fcount = fread(&sz, sizeof(sz), 1, f);
+      uint32_t uncompressed_sz;
+      uint32_t compressed_sz;
+      auto fcount = 0U;
+
+      fcount = fread(&uncompressed_sz, sizeof(uncompressed_sz), 1, f);
       if (feof(f)) {
          break;
       }
       if (fcount == 0) {
+         printf("ftell: %lu\n", ftell(f));
          throw std::runtime_error(strerror(errno));
       }
 
-      auto buf = std::make_unique<char[]>(sz);
-      fcount = fread(static_cast<void*>(buf.get()), sz, 1, f);
+      fcount = fread(&compressed_sz, sizeof(compressed_sz), 1, f);
       if (fcount == 0) {
          throw std::runtime_error(strerror(errno));
       }
 
-      auto fbc = fb::GetCountry(static_cast<void*>(buf.get()));
+      auto c_buf = std::make_unique<char[]>(compressed_sz);
+      fcount = fread(static_cast<void*>(c_buf.get()), compressed_sz, 1, f);
+      if (fcount == 0) {
+         throw std::runtime_error(strerror(errno));
+      }
+
+      auto u_buf = std::make_unique<char[]>(uncompressed_sz);
+      auto c_result = ZSTD_decompress(static_cast<void*>(u_buf.get()), uncompressed_sz, 
+                                      static_cast<void*>(c_buf.get()), compressed_sz);
+      if (ZSTD_isError(c_result)) {
+         fprintf(stderr, "Error during compression: %s\n", ZSTD_getErrorName(c_result));
+         throw std::runtime_error("Compression Error");
+      }
+      auto fbc = fb::GetCountry(static_cast<void*>(u_buf.get()));
       auto country = std::make_unique<Country>();
       country->id = fbc->id();
       country->fips = std::string(fbc->fips()->c_str());
       country->iso3 = std::string(fbc->iso3()->c_str());
       country->name = std::string(fbc->name()->c_str());
-      printf("%s - %s\n", country->name.c_str(), country->iso3.c_str());
 
       auto regions = fbc->regions();
-      for (auto ii = 0UL; ii < regions->Length(); ii++) {
-         auto region = regions->Get(ii);
-         auto fb_loops = region->loops();
-
-         std::vector<std::unique_ptr<S2Loop>> s2loops;
-         for (auto jj = 0UL; jj < fb_loops->Length(); jj++) {
-            auto fb_loop = fb_loops->Get(jj);
-            auto vertices = fb_loop->vertices();
-
-            std::vector<S2Point> points;
-            for (auto kk = 0UL; kk < vertices->Length(); kk++) {
-               const fb::Point* v = vertices->Get(kk);
-               points.emplace_back(v->x(), v->y(), v->z());
-            }
-            auto s2loop = std::make_unique<S2Loop>(points);
-            s2loops.emplace_back(std::move(s2loop));
-         }
-         auto s2poly = std::make_unique<S2Polygon>(std::move(s2loops));
-         auto t = std::unique_ptr<const S2Polygon>(std::move(s2poly));
-         auto shape = std::make_unique<S2Polygon::OwningShape>(std::move(t));
-         countries[id++] = country.get();
-         ret->Add(std::move(shape));
+      for (size_t ii = 0; ii < regions->Length(); ii++) {
+         auto poly = regions->Get(ii);
+         GEOSGeometry* geom = GEOSWKTReader_read_r(ctx, geosReader, poly->wkt()->c_str());
+         auto prepared = GEOSPrepare_r(ctx, geom);
+         auto entry = new RegionEntry{geom, prepared, country.get()};
+         GEOSSTRtree_insert(ret, geom, static_cast<void*>(entry));
       }
       country_vec.push_back(std::move(country));
    }
+   GEOSWKTReader_destroy_r(ctx, geosReader);
    return ret;
 }
 
-std::set<const Country*> countryContaining(const S2ShapeIndex *idx, double lat, double lon) {
-   auto query = MakeS2ContainsPointQuery(idx);
-   auto loc = S2Point(S2LatLng::FromDegrees(lat, lon));
-   auto shapes = query.GetContainingShapes(loc);
-   std::set<const Country*> ret;
-   for (const auto& shape: shapes) {
-      auto id = shape->id();
-      printf("id: %d\n", shape->id());
-      ret.insert(countries[id]);
+struct QCBParams {
+   std::set<const Country*>* ret;
+   GEOSGeometry* p;
+};
+
+void q_callback(void *item, void* vparams) {
+   auto c = static_cast<const RegionEntry*>(item);
+   auto params = static_cast<QCBParams*>(vparams);
+   if (GEOSPreparedIntersects_r(ctx, c->prepared, params->p)) {
+      params->ret->insert(c->country);
    }
+}
+
+std::set<const Country*> countryContaining(GEOSSTRtree *tree, double lat, double lon) {
+   std::set<const Country*> ret;
+   auto lonlat = GEOSCoordSeq_create_r(ctx,1, 2);
+   if (lonlat == nullptr) {
+      printf("error: 1\n");
+      return ret;
+   }
+   if (GEOSCoordSeq_setX_r(ctx, lonlat, 0, lon) == 0 ||
+      GEOSCoordSeq_setY_r(ctx, lonlat, 0, lat) == 0) {
+      printf("error: 2\n");
+      return ret;
+   }
+
+   auto point = GEOSGeom_createPoint_r(ctx, lonlat);
+   if (point == nullptr) {
+      printf("error: 4\n");
+      return ret;
+   }
+
+   QCBParams params{&ret, point};
+   GEOSSTRtree_query(tree, point, q_callback, &params);
+   GEOSGeom_destroy_r(ctx, point);
    return ret;
+}
+
+void cleanRegions(void* item, void*) {
+   auto c = static_cast<const RegionEntry*>(item);
+   GEOSGeom_destroy_r(ctx, c->geom);
+   GEOSPreparedGeom_destroy_r(ctx, c->prepared);
+   delete c;
+}
+
+void clean(GEOSSTRtree* idx) {
+   GEOSSTRtree_iterate(idx, cleanRegions, nullptr);
+   GEOSSTRtree_destroy_r(ctx, idx);
+   GEOS_finish_r(ctx);
 }
 
 int main() {
+   ctx = GEOS_init_r();
    auto f = fopen("data/world_map.dat", "rb");
    if (f == nullptr)  {
       perror("Error: ");
@@ -96,6 +150,8 @@ int main() {
    auto all_ccs = countryContaining(idx, 39.7392, -104.9903);
 
    for (auto c: all_ccs) {
-      printf("%s", c->name.c_str());
+      printf("%s\n", c->name.c_str());
    }
+   fclose(f);
+   clean(idx);
 }
--- src/make_world/make_world.cc
+++ src/make_world/make_world.cc
@@ -1,16 +1,9 @@
 #include "world_generated.h"
-#include "s2/s2polygon.h"
-#include "s2/s2builderutil_s2polygon_layer.h"
 #include <zstd.h>
 #include <stdio.h>
 #include <string>
 #include <cassert>
 #include <pqxx/pqxx>
-
-typedef std::pair<double, double> latlon;
-typedef std::vector<latlon> raw_loop;
-typedef std::vector<raw_loop> raw_poly;
-typedef std::vector<std::unique_ptr<S2Polygon>> Regions;
 
 struct Country {
    int id;
@@ -18,131 +11,60 @@ struct Country {
    std::string iso3;
    std::string name;
    bool is_water;
-   std::unique_ptr<Regions> regions;
 };
 
-static bool starts_with(const std::string& a, const std::string& prefix) {
-   return std::equal(prefix.begin(), prefix.end(), a.begin());
-}
+void write_countries_and_regions(FILE* f,
+                                 const std::map<int, Country>& countries,
+                                 const std::map<int, std::vector<std::string>>& region_map) {
 
-static S2Point to_point(double lat, double lon) {
-   auto safe_lat = std::min(std::max(-89.999999, lat), 89.999999);
-   auto safe_lon = std::min(std::max(-179.999999, lon), 179.999999);
-   return S2Point(S2LatLng::FromDegrees(safe_lat, safe_lon));
-}
-
-char eat_char(const std::string& str, size_t& pos, const char* expected) {
-   for (const char *x = expected; x; ++x) {
-      if (str[pos] == *x) {
-         pos++;
-         return *x;
-      }
-   }
-   std::cerr << "wkt: " << str << "\npos: " << pos << 
-      ".  Expected one of '" << expected << "', Instead found: " << str[pos] << std::endl;
-   throw std::runtime_error("Parse error");
-}
-
-std::unique_ptr<S2Loop> parse_loop(const std::string& wkt, size_t& start) {
-   std::vector<S2Point> points;
-   eat_char(wkt, start, "(");
-   while (true) {
-      size_t end;
-      double lon = std::stod(wkt.substr(start), &end);
-      start += end;
-      eat_char(wkt, start, " ");
-
-      double lat = std::stod(wkt.substr(start), &end);
-      points.emplace_back(to_point(lat, lon));
-      start+= end;
-      char next = eat_char(wkt, start, "),");
-      if (next == ')') {
-         points.pop_back();
-         auto ret = std::make_unique<S2Loop>(points);
-         return ret;
-      } 
-   }
-}
-
-std::unique_ptr<S2Polygon> parse_polygon(const std::string& wkt, size_t& start) {
-   auto loops = std::vector<std::unique_ptr<S2Loop>>();
-   while (true) {
-      eat_char(wkt, start, "(");
-      loops.emplace_back(parse_loop(wkt, start));
-      char next = eat_char(wkt, start, "),");
-      if (next == ')') {
-         auto ret = std::make_unique<S2Polygon>();
-         ret->InitNested(std::move(loops));
-         return ret;
-      }
-   }
-}
-
-std::unique_ptr<Regions> parse_wkt(const std::string& wkt) {
-   size_t start = 0;
-   auto ret = std::make_unique<Regions>();
-   if (!starts_with(wkt, "MULTIPOLYGON(")) return ret;
-   start = strlen("MULTIPOLYGON(");
-   while (true) {
-      ret->emplace_back(parse_polygon(wkt, start));
-
-      char next = eat_char(wkt, start, "),");
-      if (next == ')') {
-         start++;
-         return ret;
-      }
-   }
-}
-
-void write_country(FILE* out, const Country& country) {
    flatbuffers::FlatBufferBuilder builder(1024*1024);
-   std::vector<flatbuffers::Offset<fb::Polygon>> fb_regions;
-   for (auto& s2poly : *country.regions) {
-      std::vector<flatbuffers::Offset<fb::Loop>> fb_loops;
-      for (int ii=0; ii < s2poly->num_loops(); ++ii) {
-         const auto s2loop = s2poly->loop(ii);
-         std::vector<flatbuffers::Offset<fb::Point>> fb_points;
-         for (int jj=0; jj < s2loop->num_vertices(); ++jj) {
-            const auto& vertex = s2loop->vertex(jj);
-            const auto& data = vertex.Data();
-            auto point = fb::CreatePoint(builder, data[0], data[1], data[2]);
-            fb_points.push_back(point);
-         }
-         auto vertices = builder.CreateVector(fb_points);
-         auto fb_loop = CreateLoop(builder, vertices);
-         fb_loops.push_back(fb_loop);
+   for (auto& cpair: countries) {
+      const auto& country = cpair.second;
+      const auto& regions = region_map.find(cpair.first);
+      std::vector<flatbuffers::Offset<fb::Polygon>> fb_regions;
+      for (const auto& region: (*regions).second) {
+         auto x = builder.CreateString(region.c_str());
+         auto p = fb::CreatePolygon(builder, x);
+         fb_regions.push_back(p);
       }
-      auto fb_loop_vector = builder.CreateVector(fb_loops);
-      auto fb_poly = CreatePolygon(builder, fb_loop_vector);
-      fb_regions.push_back(fb_poly);
+
+      auto fb_poly_vector = builder.CreateVector(fb_regions);
+      auto fb_fips = builder.CreateString(country.fips);
+      auto fb_iso3 = builder.CreateString(country.iso3);
+      auto fb_name = builder.CreateString(country.name);
+
+      fb::CountryBuilder cb(builder);
+      cb.add_regions(fb_poly_vector);
+      cb.add_name(fb_name);
+      cb.add_iso3(fb_iso3);
+      cb.add_fips(fb_fips);
+      cb.add_is_water(country.is_water);
+      auto fb_country = cb.Finish();
+      builder.Finish(fb_country);
+      
+      uint32_t sz = builder.GetSize();
+      uint32_t c_size = ZSTD_compressBound(sz);
+
+      auto compressed_buf = std::make_unique<char[]>(c_size);
+      auto c_result = ZSTD_compress(compressed_buf.get(), c_size, builder.GetBufferPointer(), sz, 8);
+      if (ZSTD_isError(c_result)) {
+         fprintf(stderr, "Error during compression: %s\n", ZSTD_getErrorName(c_result));
+         throw std::runtime_error("Compression Error");
+      }
+      c_size = c_result;
+      fprintf(stdout, "%s: regions=%zd, compressed=%u\n", 
+              country.name.c_str(), regions->second.size(), c_size);
+      fwrite(&sz, sizeof(sz), 1, f);
+      fwrite(&c_size, sizeof(c_size), 1, f);
+      fwrite(compressed_buf.get(), c_size, 1, f);
+      builder.Clear();
    }
-   auto fb_poly_vector = builder.CreateVector(fb_regions);
-   auto fb_fips = builder.CreateString(country.fips);
-   auto fb_iso3 = builder.CreateString(country.iso3);
-   auto fb_name = builder.CreateString(country.name);
-
-   fb::CountryBuilder cb(builder);
-   cb.add_regions(fb_poly_vector);
-   cb.add_name(fb_name);
-   cb.add_iso3(fb_iso3);
-   cb.add_fips(fb_fips);
-   cb.add_is_water(country.is_water);
-   auto fb_country = cb.Finish();
-   builder.Finish(fb_country);
-
-   builder.Finish(fb_country);
-   uint32_t sz = builder.GetSize();
-   fwrite(&sz, sizeof(sz), 1, out);
-   fwrite(builder.GetBufferPointer(), sz, 1, out);
 }
 
-void generate_countries_from_db(FILE* out) {
-   pqxx::connection conn("host='localhost' user='bsellers' dbname='cclookup'");
-   pqxx::work txn(conn);
-
+std::map<int, Country> generate_countries_from_db(pqxx::work& txn) {
+   std::map<int, Country> ret;
    auto db_countries = 
-      txn.exec("SELECT id, fips, iso3, name, is_water, ST_AsText(border) "
-               "FROM map where fips in ('US', 'FP', 'IR') order by id");
+      txn.exec("SELECT id, fips, iso3, name, is_water FROM map");
    for (const auto& db_country: db_countries) {
       Country c;
       c.id = db_country[0].as<int>(0);
@@ -150,14 +72,22 @@ void generate_countries_from_db(FILE* out) {
       c.iso3 = db_country[2].as<std::string>("");
       c.name = db_country[3].as<std::string>("");
       c.is_water = db_country[4].as<bool>(false);
-      printf("%s: s2 ", c.name.c_str());
-      fflush(stdout);
-      c.regions = std::move(parse_wkt(db_country[5].as<std::string>("")));
-      printf("done. write "); 
-      fflush(stdout);
-      write_country(out, c);
-      printf("done.\n"); 
+      ret.emplace(c.id, c);
    }
+   return ret;
+}
+
+std::map<int, std::vector<std::string>> generate_regions_from_db(pqxx::work& txn) {
+   std::map<int, std::vector<std::string>> ret;
+   txn.exec("SET bytea_output='escape'");
+   auto db_regions = 
+      txn.exec("SELECT id, ST_AsText((ST_Dump(ST_Subdivide(border, 64))).geom) "
+               "FROM map");
+   for (const auto& row: db_regions) {
+      auto id = row[0].as<int>(0);
+      ret[id].emplace_back(row[1].as<std::string>(""));
+   }
+   return ret;
 }
 
 int main() {
@@ -167,5 +97,10 @@ int main() {
       exit(1);
    }
 
-   generate_countries_from_db(f);
+   pqxx::connection conn("host='localhost' user='bsellers' dbname='cclookup'");
+   pqxx::work txn(conn);
+   auto countries = generate_countries_from_db(txn);
+   auto regions = generate_regions_from_db(txn);
+   write_countries_and_regions(f, countries, regions);
+   fclose(f);
 }
